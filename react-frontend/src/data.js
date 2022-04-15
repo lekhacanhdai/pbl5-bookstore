98749f5780e0b831d109ddfccb1af0da88251e86
ttb2k1 <ttb2k1@gmail.com>
2022-04-15 21:01:20 +0700

    update json

---
+++ react-frontend/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://sdl-stickershop.line.naver.jp/products/0/0/1/1006363/android/stickers/319645.png;compress=true",
+    title: "CALL ME BY YOUR NAME",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita porro numquam maiores minima repellat accusantium sed teneture corrupti delectus, repudiandae officia quae repellendus utenimnon? Facilis eius cupiditate beatae!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://sdl-stickershop.line.naver.jp/products/0/0/1/1006363/android/stickers/319638.png",
+    title: "CALL ME BY YOUR NAME",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita porro numquam maiores minima repellat accusantium sed teneture corrupti delectus, repudiandae officia quae repellendus utenimnon? Facilis eius cupiditate beatae!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://sdl-stickershop.line.naver.jp/products/0/0/1/1006363/android/stickers/319628.png;compress=true",
+    title: "CALL ME BY YOUR NAME",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita porro numquam maiores minima repellat accusantium sed teneture corrupti delectus, repudiandae officia quae repellendus utenimnon? Facilis eius cupiditate beatae!",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://shereads.com/wp-content/uploads/2019/02/81RitjfpepL.jpg",
+    title: "ROMANCE",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91kCNDS7ucL.jpg",
+    title: "COMIC",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41Gz1rLAToL._SX348_BO1,204,203,200_.jpg",
+    title: "FOREIGN LANGUAGE",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://cdn0.fahasa.com/media/catalog/product/c/a/call_me_by_your_name_1_2019_01_17_08_49_24.jpg",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1582759969i/40097951.jpg",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/G/01/prime/primeinsider2/prnewesttitles/nov18/watching_you._CB480611966_.jpg",
+  },
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1582135294i/36809135.jpg",
+  },
+  {
+    id: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJLCjk8VCm2BZpAgvUxSCc9aCXF8yvlSDYFd2P425b15AbDNMPdMCMAvqQY5ZxMEdXPKY&usqp=CAU",
+  },
+  {
+    id: 6,
+    img: "https://media1.popsugar-assets.com/files/thumbor/lkMJJPPf_jCYYpdwdl1Op2wMadU/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/04/01/003/n/44498184/1a7b7fa75adf0ee3_Normal_People/i/Normal-People-by-Sally-Rooney.jpg",
+  },
+  {
+    id: 7,
+    img: "https://www.amreading.com/wp-content/uploads/lord-of-the-flies1.jpg",
+  },
+  {
+    id: 8,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCCZ1j9xqPy8QCkN9oZlwpm7wc1-QOLJbm39tYPo8pgL_M9XPctyGLa9i60UATqVR8cD4&usqp=CAU",
+  },
+];
