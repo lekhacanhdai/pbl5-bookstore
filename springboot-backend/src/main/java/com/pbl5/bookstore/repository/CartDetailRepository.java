798852d08a39e3e3893ca81b32aa4cc9b8e3cf7e
vndat00 <ngocdat1908@gmail.com>
2022-05-29 21:10:43 +0700

    [Backend][API] Fix error missing code in CartDetailRepository.java file when pulling

--- springboot-backend/src/main/java/com/pbl5/bookstore/repository/CartDetailRepository.java
+++ springboot-backend/src/main/java/com/pbl5/bookstore/repository/CartDetailRepository.java
@@ -1,9 +1,22 @@
 package com.pbl5.bookstore.repository;
 
 import com.pbl5.bookstore.model.CartDetail;
+import com.pbl5.bookstore.model.CartDetailKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
+    Optional<CartDetail> getCartDetailById(CartDetailKey cartDetailKey);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM CartDetail c where " +
+            "c.id.bookId = ?1 and c.id.cartId = ?2")
+    void deleteCartDetailByKey(long bookId, long cartId);
 }
